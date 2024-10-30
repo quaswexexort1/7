8ce859886e0eb8d60bc09821eefde5913835e298
SHEL <SHEL@DESKTOP-6SFOS1M>
2024-10-30 14:51:46 +0200

    Добавьте файлы проекта.

---
+++ Операторы цикла с предусловием и постусловием/Program.cs
@@ -0,0 +1,20 @@
+﻿try
+{
+    Console.WriteLine("Введите последовательность чисел(Введите 0 для завершения ввода)");
+    int first = 1;
+    int second;
+    do
+    {
+        second = int.Parse(Console.ReadLine());
+        if (second != 0)
+        {
+            first *= second;
+        }
+    } while (second != 0);
+
+    Console.WriteLine($"Произведение чисел: {first}");
+}
+catch(Exception ex) 
+{
+    Console.WriteLine(ex.Message);
+}
